e17dba26b666d4adaeb94cf4343ccc8820f5693a
opstodevops <opstodevops@outlook.com>
2021-11-16 10:38:18 +1300

    rename

--- create_import_cert.sh
+++ create_import_cert.sh
@@ -10,27 +10,27 @@ cd easy-rsa/easyrsa3
 ./easyrsa init-pki
 ./easyrsa build-ca nopass
 ./easyrsa build-server-full server nopass
-./easyrsa build-server-full xero-clientvpn-test nopass
+./easyrsa build-server-full zero-clientvpn-test nopass
 mkdir custom_folder/
 cp pki/ca.crt custom_folder/
 cp pki/issued/server.crt custom_folder/
 cp pki/private/server.key custom_folder/
-cp pki/issued/xero-clientvpn-test.crt custom_folder
-cp pki/private/xero-clientvpn-test.key custom_folder/
+cp pki/issued/zero-clientvpn-test.crt custom_folder
+cp pki/private/zero-clientvpn-test.key custom_folder/
 cd custom_folder/
 
 # Import server cert to ACM
 aws acm import-certificate \
-  --certificate fileb://xero-clientvpn-test.crt \
-  --private-key fileb://xero-clientvpn-test.key \
+  --certificate fileb://zero-clientvpn-test.crt \
+  --private-key fileb://zero-clientvpn-test.key \
   --certificate-chain fileb://ca.crt \
-  --tags Key=Name,Value=xero-clientvpn-test \
+  --tags Key=Name,Value=zero-clientvpn-test \
   --region $REGION
 aws acm import-certificate \
   --certificate fileb://server.crt \
   --private-key fileb://server.key \
   --certificate-chain fileb://ca.crt \
-  --tags Key=Name,Value=xero-clientvpn-test \
+  --tags Key=Name,Value=zero-clientvpn-test \
   --region $REGION
 cd $ROOT_DIR
 rm -rf easy-rsa
